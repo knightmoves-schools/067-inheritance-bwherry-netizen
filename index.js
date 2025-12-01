b92bec7c3a3ea813d196c650bca88d1ebfc181e4
Brendon W. <bwherry@midland.k12.ia.us>
2025-12-01 13:36:58 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,37 +1,40 @@
-class Agable{
-    constructor(yearEstablished, birthYear, year,){
-        this.birthYear = birthYear;
-        this.yearEstablished = yearEstablished;
+class Agable {
+    constructor(year) {
         this.year = year;
-    }    
-     
+    }
+
+    age() {
+        const currentYear = new Date().getFullYear();
+        return currentYear - this.year;
+    }
 }
 
 class Company extends Agable {
     constructor(name, taxId, yearEstablished, taxRate){
+        super(yearEstablished);
         this.name = name;
         this.id = taxId;
-        super(yearEstablished)
         this.taxRate = taxRate;
     }
 }
 
-class Person extends Agable{
+class Person extends Agable {
     constructor(name, ssn, birthYear, taxRate){
+        super(birthYear);
         this.name = name;
         this.id = ssn;
-        super(birthYear)
         this.taxRate = taxRate;
     }
 }
 
-class Car extends Agable{
+class Car extends Agable {
     constructor(model, vin, year){
+        super(year);
         this.name = model;
         this.id = vin;
-        super(year)
     }
 }
+
 
 
 
